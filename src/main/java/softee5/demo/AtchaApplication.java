981a4888f1861ca362d252a4c15a502c3356ae09
admin <admin@adminui-MacBookPro.local>
2024-01-04 10:25:47 +0900

    first commit

---
+++ src/main/java/softee5/demo/AtchaApplication.java
@@ -0,0 +1,13 @@
+package softee5.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtchaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/softee5/demo/AtchaApplicationTests.java
@@ -0,0 +1,13 @@
+package softee5.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
