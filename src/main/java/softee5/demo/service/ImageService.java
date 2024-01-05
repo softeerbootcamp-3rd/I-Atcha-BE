b7314ecac9f660b8f105c0e71ffbcadf0437cde0
win9 <krrr883@gmail.com>
2024-01-05 15:05:38 +0900

    🎉[FEAT] : S3Uploader 업로드로직 구현

--- src/main/java/softee5/demo/service/ImageService.java
+++ src/main/java/softee5/demo/service/ImageService.java
@@ -1,11 +1,24 @@
 package softee5.demo.service;
 
+import com.amazonaws.services.s3.AmazonS3Client;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import softee5.demo.repository.ImageRepository;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ImageService {
     private final ImageRepository imageRepository;
+
+//    public String upload(MultipartFile multipartFile) throws IOException {
+//
+//    }
+
 }
