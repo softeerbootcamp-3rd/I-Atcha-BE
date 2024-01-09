eec778295863292832187ae405e4d8d63c23d99e
win9 <krrr883@gmail.com>
2024-01-09 09:56:47 +0900

    🎉[FEAT] : 테스팅용 로그 추가

--- src/main/java/softee5/demo/controller/TestController.java
+++ src/main/java/softee5/demo/controller/TestController.java
@@ -1,6 +1,7 @@
 package softee5.demo.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping
 @CrossOrigin(origins = "*", allowedHeaders = "*")
+@Slf4j
 public class TestController {
 
     @GetMapping("/")
     public String home() {
+        log.info("home");
         return "server running";
     }
 }
