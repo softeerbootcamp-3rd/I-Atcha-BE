38a39731fcc3f0a765e8b4fa4522b4cb655c508c
Yang-soeun <soeun8636@naver.com>
2024-01-10 00:58:46 +0900

    🛠[FIX] 히스토리 상세보기 dto 수정

--- src/main/java/softee5/demo/dto/response/HistoryDetailResponseDto.java
+++ src/main/java/softee5/demo/dto/response/HistoryDetailResponseDto.java
@@ -20,16 +20,16 @@ public class HistoryDetailResponseDto {
     private String parkingDate;
     private String parkingTime;
     private String paidFee;
-    private List<String> link;
+    private String link;
     private ParkingLot parkingLot;
     private String content;
 
-    public static HistoryDetailResponseDto getHistoryDetailResponseDto(History history, String fee, List<String> link){
+    public static HistoryDetailResponseDto getHistoryDetailResponseDto(History history, String fee, String link){
         return HistoryDetailResponseDto.builder()
                 .parkingDate(formatDate(history.getCreateTime()))
                 .parkingTime(history.getParkingTime())
                 .paidFee(history.getPaidFee())
-                .link(link)
+                .link(link == null ? "" : link)
                 .parkingLot(ParkingLot.getParkingLotDto(fee,history.getParking()))
                 .content(history.getMemo())
                 .build();
